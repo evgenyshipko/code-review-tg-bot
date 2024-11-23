b401aa783f7295ad870c472b1704f5fda3393cfb
Евгений Шипко <e.shipko@ritm.media>
2024-11-24 00:12:13 +0700

    добавил глобальную обработку ошибки в цикле бота

--- main/main.go
+++ main/main.go
@@ -25,6 +25,7 @@ func main() {
 	BotToken := os.Getenv("BOT_TOKEN")
 
 	bot, err := tg.NewBotAPI(BotToken)
+	// TODO: законсолить нормально ошибку
 	if err != nil {
 		panic(err)
 	}
@@ -36,92 +37,88 @@ func main() {
 	u.Timeout = 60
 	updates := bot.GetUpdatesChan(u)
 
-	rxStrict := xurls.Strict
-
 	getReviewers := reviewers.MakeGetReviewerFuncWithMemo(bot.GetChatMember, 2)
 
-baseCycle:
 	for update := range updates {
-
-		//defer func() {
-		//	if r := recover(); r != nil {
-		//
-		//	}
-		//}()
-
-		if update.Message == nil {
-			continue
-		}
-
-		if strings.Contains(update.Message.Text, bot.Self.UserName) {
-			fmt.Println("[%s] %s", update.Message.From.UserName, update.Message.Text)
-
-			//TODO: что за параметр -1?
-			urls := rxStrict.FindAllString(update.Message.Text, -1)
-
-			fmt.Println("urls", urls)
-
-			if len(urls) == 0 {
-				sendNewMessage("Необходимо добавить ссылку на merge request", bot, update)
-				continue baseCycle
-			}
-
-			globalMsg := "Требуется ревью:"
-
-			for _, url := range urls {
-				projectName, mergeRequestId, parseErr := parser.ParseGitlabURL(url)
-				if parseErr != nil {
-					sendNewMessage(parseErr.Error(), bot, update)
-					continue baseCycle
-				}
-
-				//TODO: projectId - неизменяемая информация, поэтому надо уметь результат этой ручки мемоизировать
-				projectId, err := requests.GetProjectId(projectName)
-				fmt.Println("projectId", projectId)
-				if err != nil {
-					sendNewMessage("Что-то пошло не так: "+err.Error(), bot, update)
-					continue baseCycle
-				}
-
-				mergeRequestData, err := requests.GetMergeRequestData(projectId, mergeRequestId)
-				if err != nil {
-					sendNewMessage("Что-то пошло не так: "+err.Error(), bot, update)
-					continue baseCycle
-				}
-
-				if mergeRequestData.HasConflicts {
-					msg := fmt.Sprintf("Для начала нужно пофиксить <a href=\"%s\">конфликты</a>", url)
-					sendNewMessage(msg, bot, update)
-					continue baseCycle
-				}
-
-				globalMsg += "\n" + fmt.Sprintf("<a href=\"%s\">%s</a>", url, mergeRequestData.Title)
-			}
-
-			reviewersList, err := getReviewers(update.Message.Chat.ID)
-			if err != nil {
-				sendNewMessage("Что-то пошло не так: "+err.Error(), bot, update)
-				continue baseCycle
-			}
-
-			//panic("mother fucker")
-
-			fmt.Println("reviewersList[0].User", reviewersList[0].User)
-
-			globalMsg += "\nРевьюеры: "
-
-			for _, reviewer := range reviewersList {
-				globalMsg += "@" + reviewer.User.UserName + " "
-			}
-
-			sendNewMessage(globalMsg, bot, update)
-		}
+		mainLoopFunc(update, bot, getReviewers)
 	}
 
 }
 
-// обрабатывать глобальнуную панику - приложение не должно падать
-// настроить единое логгирование
+//TODO: настроить единое логгирование
+
+func mainLoopFunc(update tg.Update, bot *tg.BotAPI, reviewerFunc reviewers.GetReviewerFunc) {
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Println("Recovered in mainLoopFunc", r)
+			sendNewMessage(fmt.Sprintf("Что-то пошло не так: %s", r), bot, update)
+		}
+	}()
+
+	if update.Message == nil {
+		return
+	}
+
+	if strings.Contains(update.Message.Text, bot.Self.UserName) {
+		fmt.Println("[%s] %s", update.Message.From.UserName, update.Message.Text)
+
+		//TODO: что за параметр -1?
+		urls := xurls.Strict.FindAllString(update.Message.Text, -1)
+
+		fmt.Println("urls", urls)
+
+		if len(urls) == 0 {
+			sendNewMessage("Необходимо добавить ссылку на merge request", bot, update)
+			return
+		}
+
+		globalMsg := "Требуется ревью:"
+
+		for _, url := range urls {
+			projectName, mergeRequestId, parseErr := parser.ParseGitlabURL(url)
+			if parseErr != nil {
+				sendNewMessage(parseErr.Error(), bot, update)
+				return
+			}
+
+			//TODO: projectId - неизменяемая информация, поэтому надо уметь результат этой ручки мемоизировать
+			projectId, err := requests.GetProjectId(projectName)
+			fmt.Println("projectId", projectId)
+			if err != nil {
+				sendNewMessage("Что-то пошло не так: "+err.Error(), bot, update)
+				return
+			}
+
+			mergeRequestData, err := requests.GetMergeRequestData(projectId, mergeRequestId)
+			if err != nil {
+				sendNewMessage("Что-то пошло не так: "+err.Error(), bot, update)
+				return
+			}
+
+			if mergeRequestData.HasConflicts {
+				msg := fmt.Sprintf("Для начала нужно пофиксить <a href=\"%s\">конфликты</a>", url)
+				sendNewMessage(msg, bot, update)
+				return
+			}
+
+			globalMsg += "\n" + fmt.Sprintf("<a href=\"%s\">%s</a>", url, mergeRequestData.Title)
+		}
+
+		reviewersList, err := reviewerFunc(update.Message.Chat.ID)
+		if err != nil {
+			sendNewMessage("Что-то пошло не так: "+err.Error(), bot, update)
+			return
+		}
+
+		globalMsg += "\nРевьюеры: "
+
+		for _, reviewer := range reviewersList {
+			globalMsg += "@" + reviewer.User.UserName + " "
+		}
+
+		sendNewMessage(globalMsg, bot, update)
+	}
+}
 
 func sendNewMessage(message string, bot *tg.BotAPI, update tg.Update) {
 	msg := tg.NewMessage(update.Message.Chat.ID, message)
